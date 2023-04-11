86f932ff9993060b5fdd5d6072561a08dfc59c37
Direct-A <songyicheng0@gmail.com>
2023-04-12 00:35:08 +0800

    翻新目录结构，优化双拼为音形

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Author: syc
+# Date: 2023-04-11 23:48:18
+# Version: 0.1
+# Description: 清理残余文件
+
+LIST=(default.yaml huayu.dict.yaml punctuator.yaml keybindings.yaml)
+
+for i in `ls ./target`; do
+  if [[ ! " ${LIST[@]} " =~ " ${i} " ]]; then
+    rm -r ./target/${i}
+  fi
+done
